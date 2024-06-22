9b8a936de7e109bc49e36eb4fa6213528b78241f
Dilligi <as6053825@gmail.com>
2024-06-22 23:12:12 +0300

    delete old method

--- voice_to_text.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import whisper
-
-def voice_to_text(file_path: str) -> str:
-    model = whisper.load_model("base")
-    result = model.transcribe(file_path, fp16=False)
-    return result["text"]
